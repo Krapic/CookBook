1ff27cc294963c496bb6e5032e8104fed638bac8
Frane Krapić <krapicf@gmail.com>
2026-03-14 18:29:06 +0100

    feat: Add `Resource` sealed class for UI state management

--- app/src/main/java/com/rma/cookbook/util/Resource.kt
+++ app/src/main/java/com/rma/cookbook/util/Resource.kt
@@ -1,2 +1,10 @@
 package com.rma.cookbook.util
 
+<<<<<<< Updated upstream
+=======
+sealed class Resource<T> {
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Error<T>(val message: String) : Resource<T>()
+    class Loading<T> : Resource<T>()
+}
+>>>>>>> Stashed changes
