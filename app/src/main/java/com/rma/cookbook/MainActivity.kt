cbf3f57f0087524e13f95f7ad689cebf5caa3a16
Frane Krapić <krapicf@gmail.com>
2026-03-14 18:29:07 +0100

    refactor: Integrate `AppNavHost` into `MainActivity`

--- app/src/main/java/com/rma/cookbook/MainActivity.kt
+++ app/src/main/java/com/rma/cookbook/MainActivity.kt
@@ -3,45 +3,16 @@ package com.rma.cookbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.rma.cookbook.ui.navigation.AppNavHost
 import com.rma.cookbook.ui.theme.CookBookTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CookBookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavHost()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CookBookTheme {
-        Greeting("Android")
     }
 }
