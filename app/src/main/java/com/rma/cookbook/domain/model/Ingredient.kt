030cb179f95c2c9dd16171e6386dbae12d4e78ef
Frane Krapić <krapicf@gmail.com>
2026-03-14 18:29:05 +0100

    feat: Define core domain models for Cookbook entities

--- app/src/main/java/com/rma/cookbook/domain/model/Ingredient.kt
+++ app/src/main/java/com/rma/cookbook/domain/model/Ingredient.kt
@@ -1,2 +1,10 @@
 package com.rma.cookbook.domain.model
 
+<<<<<<< Updated upstream
+=======
+data class Ingredient(
+    val name: String = "",
+    val amount: String = "",
+    val unit: String = ""
+)
+>>>>>>> Stashed changes
--- app/src/main/java/com/rma/cookbook/domain/model/Step.kt
+++ app/src/main/java/com/rma/cookbook/domain/model/Step.kt
@@ -1,2 +1,9 @@
 package com.rma.cookbook.domain.model
 
+<<<<<<< Updated upstream
+=======
+data class Step(
+    val order: Int = 0,
+    val description: String = ""
+)
+>>>>>>> Stashed changes
