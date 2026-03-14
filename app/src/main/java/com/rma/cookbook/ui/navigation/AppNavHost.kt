e62f5e1b3bb57bc901d806859cad76c7a9215c48
Frane Krapić <krapicf@gmail.com>
2026-03-14 18:29:06 +0100

    feat: Implement Jetpack Compose Navigation infrastructure

--- app/src/main/java/com/rma/cookbook/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/rma/cookbook/ui/navigation/AppNavHost.kt
@@ -1,2 +1,36 @@
 package com.rma.cookbook.ui.navigation
 
+<<<<<<< Updated upstream
+=======
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rma.cookbook.ui.screens.auth.LoginScreen
+import com.rma.cookbook.ui.screens.auth.RegisterScreen
+import com.rma.cookbook.ui.screens.home.HomeScreen
+import com.rma.cookbook.ui.screens.recipe.AddRecipeScreen
+
+@Composable
+fun AppNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+        composable(Screen.Login.route) {
+            LoginScreen()
+        }
+        composable(Screen.Register.route) {
+            RegisterScreen()
+        }
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+        composable(Screen.AddRecipe.route) {
+            AddRecipeScreen()
+        }
+    }
+}
+>>>>>>> Stashed changes
