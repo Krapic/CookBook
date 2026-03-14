a417aee3a1d7780c9b71f6f24bf9087cc58960bd
Krapic <78276718+Krapic@users.noreply.github.com>
2026-03-14 19:40:51 +0100

    Remove Android app module and project configs
    Delete the Android app module and associated project metadata: removes .idea IDE settings, app/ sources (Kotlin code, domain models, UI, navigation), resources (res/, mipmap/, drawables, xml), manifests, tests, app build files and proguard rules, README, and Gradle/ wrapper/settings files. Consolidates removal of the existing Android project contents and related configuration files.

--- app/src/main/java/com/rma/cookbook/domain/model/Step.kt
+++
@@ -1,9 +0,0 @@
-package com.rma.cookbook.domain.model
-
-<<<<<<< Updated upstream
-=======
-data class Step(
-    val order: Int = 0,
-    val description: String = ""
-)
->>>>>>> Stashed changes
--- app/src/main/java/com/rma/cookbook/ui/screens/recipe/RecipeDetailScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.rma.cookbook.ui.screens.recipe
-
