7b8e34f1e434e568384909a32bf44e082fac8112
Frane Krapić <krapicf@gmail.com>
2026-03-15 01:06:48 +0100

    feat: Define repository interfaces for Authentication and Recipe management

--- app/src/main/java/com/rma/cookbook/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/rma/cookbook/domain/repository/AuthRepository.kt
@@ -1,2 +1,14 @@
 package com.rma.cookbook.domain.repository
 
+<<<<<<< Updated upstream
+=======
+import com.rma.cookbook.domain.model.User
+import com.rma.cookbook.util.Resource
+
+interface AuthRepository {
+    suspend fun login(email: String, password: String): Resource<User>
+    suspend fun register(email: String, password: String, displayName: String): Resource<User>
+    fun logout()
+    fun getCurrentUser(): User?
+}
+>>>>>>> Stashed changes
--- app/src/main/java/com/rma/cookbook/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/rma/cookbook/domain/repository/RecipeRepository.kt
@@ -1,2 +1,15 @@
 package com.rma.cookbook.domain.repository
 
+<<<<<<< Updated upstream
+=======
+import com.rma.cookbook.domain.model.Recipe
+import com.rma.cookbook.util.Resource
+
+interface RecipeRepository {
+    suspend fun getRecipes(): Resource<List<Recipe>>
+    suspend fun getRecipeById(id: String): Resource<Recipe>
+    suspend fun createRecipe(recipe: Recipe): Resource<String>
+    suspend fun updateRecipe(recipe: Recipe): Resource<Unit>
+    suspend fun deleteRecipe(id: String): Resource<Unit>
+}
+>>>>>>> Stashed changes
