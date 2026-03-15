f5274c27c6cdd3b646ad7874835e6476ce4af965
Frane Krapić <krapicf@gmail.com>
2026-03-15 01:06:50 +0100

    feat: Add placeholder UI screens for authentication, home, and recipe features

--- app/src/main/java/com/rma/cookbook/ui/screens/recipe/AddRecipeScreen.kt
+++ app/src/main/java/com/rma/cookbook/ui/screens/recipe/AddRecipeScreen.kt
@@ -1,2 +1,12 @@
 package com.rma.cookbook.ui.screens.recipe
 
+<<<<<<< Updated upstream
+=======
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddRecipeScreen() {
+    Text("Add Recipe Screen")
+}
+>>>>>>> Stashed changes
--- app/src/main/java/com/rma/cookbook/ui/screens/recipe/RecipeDetailScreen.kt
+++ app/src/main/java/com/rma/cookbook/ui/screens/recipe/RecipeDetailScreen.kt
@@ -1,2 +1,12 @@
 package com.rma.cookbook.ui.screens.recipe
 
+<<<<<<< Updated upstream
+=======
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeDetailScreen() {
+    Text("Recipe Detail Screen")
+}
+>>>>>>> Stashed changes
