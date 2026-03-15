f0c97e332d2b62c7ff9cc347417398de71be3044
Frane Krapić <krapicf@gmail.com>
2026-03-15 01:06:48 +0100

    build: Add AndroidX Navigation Compose and Lifecycle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.navigation:navigation-compose:2.9.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
 }
